c631e9a7ec2ffea958e7a144676179e8a9eab37f
Wataru Miyakoshi <wtrmiya@gmail.com>
2022-04-28 04:22:31 +0900

    apply .gitignore

--- UITableView+RxSwift/MyCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MyCell.swift
-//  UITableView+RxSwift
-//
-//  Created by Wataru Miyakoshi on 2022/04/28.
-//
-
-import Foundation
--- UITableView+RxSwift/TableViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TableViewController.swift
-//  UITableView+RxSwift
-//
-//  Created by Wataru Miyakoshi on 2022/04/28.
-//
-
-import Foundation
